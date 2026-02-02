f4f2a147d27cfc838434014b3bfdc91f9c2c895c
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-02 21:00:32 +0545

    Added:Exam list

--- exams/urls.py
+++ exams/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.ExamListView.as_view(), name='exam_list'),
     path('add/', views.ExamCreateView.as_view(), name='exam_add'),
     path('<int:pk>/', views.ExamDetailView.as_view(), name='exam_detail'),
+    path('<int:exam_id>/question/add/', views.QuestionCreateView.as_view(), name='question_add'),
 ]
