0cafe3a1dbd6ded47e469dbe9b8e1d5cb1480d7b
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-02 20:59:56 +0545

    Added:Student detail

--- exams/views.py
+++ exams/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Exam
+from .models import Exam, Question
+from students.models import Subject
 
 
 class ExamListView(LoginRequiredMixin, ListView):
@@ -11,6 +12,7 @@ class ExamListView(LoginRequiredMixin, ListView):
     template_name = 'exams/exam_list.html'
     context_object_name = 'exams'
     paginate_by = 20
+    ordering = ['-exam_date']
 
 
 class ExamDetailView(LoginRequiredMixin, DetailView):
@@ -21,7 +23,7 @@ class ExamDetailView(LoginRequiredMixin, DetailView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['questions'] = self.object.questions.select_related('subject').all()
+        context['questions'] = self.object.questions.select_related('subject').order_by('question_number')
         return context
 
 
@@ -35,3 +37,26 @@ class ExamCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
+
+
+class QuestionCreateView(LoginRequiredMixin, CreateView):
+    """Add question to exam"""
+    model = Question
+    template_name = 'exams/question_form.html'
+    fields = ['subject', 'question_number', 'question_text', 'question_type', 'marks', 'answer_key']
+    
+    def dispatch(self, request, *args, **kwargs):
+        self.exam = get_object_or_404(Exam, pk=kwargs['exam_id'])
+        return super().dispatch(request, *args, **kwargs)
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['exam'] = self.exam
+        return context
+        
+    def form_valid(self, form):
+        form.instance.exam = self.exam
+        return super().form_valid(form)
+        
+    def get_success_url(self):
+        return reverse('exams:exam_detail', kwargs={'pk': self.exam.pk})
